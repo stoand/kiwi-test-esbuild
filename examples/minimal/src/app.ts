b17ce297cccdd317a64bfb0c549cb45ae21d27f9
Andreas Stocker <shadowstep7@gmail.com>
2023-03-03 20:21:39 +0100

    run update-esbuild once to prevent hanging on error

--- examples/minimal/src/app.ts
+++ examples/minimal/src/app.ts
@@ -4,9 +4,10 @@ console.log(React);
 
 // fail.now = 1
 
-if (Math.PI + 1 = 3 || Math.PI + 1 = 4) {
-  console.log("no way!");
-}
+// this causes a build-time error
+// if (Math.PI + 1 = 3 || Math.PI + 1 = 4) {
+//   console.log("no way!");
+// }
 
 export function add(a: number, b: number): number {
     return a + b;
